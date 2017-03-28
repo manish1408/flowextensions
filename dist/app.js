caf9bd59f83d10546d2e498d6eaa21b018d33c79
Manish Gautam <manish.gautam.india@gmail.com>
2017-03-28 13:32:31 +0530

    Connected to xml data

--- dist/app.js
+++ dist/app.js
@@ -6,7 +6,7 @@ angular.module('app', ['flowchart','ui.tree'])
     NodeTemplatePathProvider.setTemplatePath("flowchart/node.html");
   })
 
-  .controller('AppCtrl', function AppCtrl($scope, prompt, Modelfactory, flowchartConstants) {
+  .controller('AppCtrl', function AppCtrl($scope, prompt, Modelfactory, flowchartConstants, $http) {
 
     var deleteKeyCode = 46;
     var ctrlKeyCode = 17;
@@ -16,6 +16,63 @@ angular.module('app', ['flowchart','ui.tree'])
     var nextConnectorID = 20;
     var ctrlDown = false;
 
+    $scope.processData = function() {
+      $http.get("xmldata.json")
+      .then(function(response) {
+        var navigationdata =[];
+        var template = {
+        'id': 1,
+        'title': 'node1',
+        'type': 'flow',
+        'nodes': [
+          {
+            'id': 11,
+            'title': 'node1.1',
+            'nodes': []
+          }
+        ]
+      };
+          $scope.allxmlData = response.data;
+          console.log($scope.allxmlData);
+
+         $scope.allxmlData.FlowInformation.Flows.forEach(function(element) {
+           template.id = element.id;
+           template.title = element.FlowName;
+           template.type = 'flow';
+           template.tooltip = 'Show Diagram for ' + element.FlowName;
+
+            var allNodes = [];
+            element.Node.forEach(function(el) {
+
+
+              allNodes.push({
+                'id': el.NodeType,
+                'title': el.NodeType,
+                'tooltip' : 'Add Node ' +el.NodeType + ' to canvas diagram',
+                'type' : 'node',
+                'nodes': [{
+                  'id': el[Object.keys(el)[1]].id,
+                 'title': el[Object.keys(el)[1]].Name,
+                 'type' : 'attr'
+                }]
+              });
+            }, this);
+
+           
+
+            template.nodes = allNodes;
+
+            navigationdata.push(template);
+
+         }, this);
+
+
+         console.log(navigationdata);
+         $scope.data = navigationdata;
+      });
+    };
+
+    $scope.processData();
     var model = {
       nodes: [
         {
@@ -279,13 +336,28 @@ angular.module('app', ['flowchart','ui.tree'])
         $scope.data.splice(0, 0, a);
       };
 
-      $scope.newSubItem = function (scope) {
+      $scope.newSubItem = function (scope, type) {
         var nodeData = scope.$modelValue;
-        nodeData.nodes.push({
-          id: nodeData.id * 10 + nodeData.nodes.length,
-          title: nodeData.title + '.' + (nodeData.nodes.length + 1),
-          nodes: []
-        });
+        if (type === 'node') {
+           nodeData.nodes.push({
+            id: nodeData.id * 10 + nodeData.nodes.length,
+            title: nodeData.title + '.' + (nodeData.nodes.length + 1),
+            type: 'attr'
+          });
+        } else if (type === 'flow'){
+             nodeData.nodes.push({
+            id: nodeData.id * 10 + nodeData.nodes.length,
+            title: nodeData.title + '.' + (nodeData.nodes.length + 1),
+            type: 'node',
+            nodes: []
+          });
+        } else {
+             nodeData.nodes.push({
+            id: nodeData.id * 10 + nodeData.nodes.length,
+            title: nodeData.title + '.' + (nodeData.nodes.length + 1),
+            nodes: []
+          });
+        }
       };
 
       $scope.collapseAll = function () {
@@ -296,52 +368,52 @@ angular.module('app', ['flowchart','ui.tree'])
         $scope.$broadcast('angular-ui-tree:expand-all');
       };
       
-   $scope.data = [{
-        'id': 1,
-        'title': 'node1',
-        'nodes': [
-          {
-            'id': 11,
-            'title': 'node1.1',
-            'nodes': [
-              {
-                'id': 111,
-                'title': 'node1.1.1',
-                'nodes': []
-              }
-            ]
-          },
-          {
-            'id': 12,
-            'title': 'node1.2',
-            'nodes': []
-          }
-        ]
-      }, {
-        'id': 2,
-        'title': 'node2',
-        'nodrop': true, // An arbitrary property to check in custom template for nodrop-enabled
-        'nodes': [
-          {
-            'id': 21,
-            'title': 'node2.1',
-            'nodes': []
-          },
-          {
-            'id': 22,
-            'title': 'node2.2',
-            'nodes': []
-          }
-        ]
-      }, {
-        'id': 3,
-        'title': 'node3',
-        'nodes': [
-          {
-            'id': 31,
-            'title': 'node3.1',
-            'nodes': []
-          }
-        ]
-      }];
+  //  $scope.data = [{
+  //       'id': 1,
+  //       'title': 'node1',
+  //       'nodes': [
+  //         {
+  //           'id': 11,
+  //           'title': 'node1.1',
+  //           'nodes': [
+  //             {
+  //               'id': 111,
+  //               'title': 'node1.1.1',
+  //               'nodes': []
+  //             }
+  //           ]
+  //         },
+  //         {
+  //           'id': 12,
+  //           'title': 'node1.2',
+  //           'nodes': []
+  //         }
+  //       ]
+  //     }, {
+  //       'id': 2,
+  //       'title': 'node2',
+  //       'nodrop': true, // An arbitrary property to check in custom template for nodrop-enabled
+  //       'nodes': [
+  //         {
+  //           'id': 21,
+  //           'title': 'node2.1',
+  //           'nodes': []
+  //         },
+  //         {
+  //           'id': 22,
+  //           'title': 'node2.2',
+  //           'nodes': []
+  //         }
+  //       ]
+  //     }, {
+  //       'id': 3,
+  //       'title': 'node3',
+  //       'nodes': [
+  //         {
+  //           'id': 31,
+  //           'title': 'node3.1',
+  //           'nodes': []
+  //         }
+  //       ]
+  //     }];
   });
